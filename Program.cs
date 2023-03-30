a5c53e08aa6f1824bcbb7b133376b87a1c046e3f
Janavandevelde20 <r0938194@student.thomasmore.be>
2023-03-30 12:00:20 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask the user to enter a decimal number
+            Console.Write("Enter a decimal number: ");
+
+            //convert the given string to an integer
+            int decimalNumber = Convert.ToInt32((Console.ReadLine()));
+
+            //convert the decimal number to binary with using a while loop
+            int[] binaryDigits = new int[32]; // max 32-bit binary number
+            int i = 0;
+            while (decimalNumber > 0)
+            {
+                //store the binary digits in an array
+                binaryDigits[i] = decimalNumber % 2;
+                decimalNumber = decimalNumber / 2;
+                i++;
+            }
+
+            //print the binary number in reverse order
+            Console.Write("The binary representation for this number is: ");
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.Write(binaryDigits[j]);
+            }
+            Console.WriteLine();
+        }
+        }
+    }
